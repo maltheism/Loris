5c13740f2bbc826281ac104832c8d83e4ea1f376
Alizée Bellerose <misotheism@users.noreply.github.com>
2020-05-06 15:47:48 -0400

    files from john's pr

--- test/run-php-linter.sh
+++ test/run-php-linter.sh
@@ -1,21 +1,39 @@
 #!/usr/bin/env bash
-set -e
+set -euo pipefail
 
 # Run PHP -l on everything to ensure there's no syntax
 # errors.
-for i in `ls php/libraries/*.class.inc modules/*/php/* modules/*/ajax/* htdocs/*.php htdocs/*/*.php`;
-do
-  php -l $i || exit $?;
-done
+find docs modules htdocs php src tools -name '*.class.inc' -print0 -o -name '*.php' -print0 |xargs -0 -n1 php -l >/dev/null
 
-# Run PHPCS on the entire libraries directory.
-vendor/bin/phpcs --standard=docs/LorisCS.xml php/libraries php/exceptions php/installer || exit $?;
-vendor/bin/phpcs --standard=docs/LorisCS.xml --extensions=php/php htdocs || exit $?;
+# Run PHPCS on all .php and .inc files in folders:
+# php/
+# htdocs/
+# modules/
 
-# Run PHPCS on some scripts  -- fixing the files format later
-# vendor/bin/phpcs --standard=docs/LorisCS.xml tools/CouchDB_Confirm_Integrity.php
+# Also run PHPCS on all tools/ scripts in this array
+declare -a tools_list=(
+    'assign_missing_instruments.php'
+    'delete_ignored_conflicts.php'
+    'detect_duplicated_commentids.php'
+    'generic_includes.php'
+    'importers/CandidateImporter.php'
+    'importers/DataImporter.class.inc'
+    'importers/InstrumentImporter.php'
+    'importers/VisitImporter.php'
+    'importers/openScienceDataImporter.php'
+    'exporters/dataExtractor.php'
+    'populate_examiners_psc_rel.php'
+    'raisinbread_refresh.php'
+    'resetpassword.php'
+    'setconfig.php'
+    'single_use/Cleanup_multiple_firstVisits.php'
+)
+vendor/bin/phpcs --standard=test/LorisCS.xml --extensions=php,inc php/ htdocs/ modules/ "${tools_list[@]/#/tools/}" || exit $?;
 
-# Run PHPCS on all modules
-vendor/bin/phpcs --standard=docs/LorisCS.xml --extensions=php/php,inc/php modules/ || exit $?;
+# Run PHPCS on src/ directory using a different ruleset conforming to PSR2.
+vendor/bin/phpcs --standard=test/SrcCS.xml --extensions=php/php src/ || exit $?;
 
-vendor/bin/phpmd php/libraries text docs/LorisPHPMD.xml || exit $?;
+vendor/bin/phpmd php/,modules/,src/ text 'test/LorisPHPMD.xml' || exit $?;
+
+# Run PHPStan on php/ and modules/
+vendor/bin/phpstan analyse --level max -c ./test/phpstan-loris.neon --error-format table php/ modules/ || exit $?
