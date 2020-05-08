a748829f4275360001d469484eea31843d9a979a
Cecile Madjar <cecile.madjar@gmail.com>
2020-05-08 14:30:13 -0400

    fix link for event files in BIDS

--- htdocs/api/v0.0.3-dev/projects/Bids.php
+++ htdocs/api/v0.0.3-dev/projects/Bids.php
@@ -184,7 +184,7 @@ class Bids extends \Loris\API\APIBase
                         "/candidates/$candid/$session/images/$minc_name/bids/$file_name.bvec";
                 }
 
-                if (strpos('_task-', $file_name)) {
+                if (preg_match('/_task-/', $file_name)) {
                     $final_array_to_return['EventLink'] =
                         "/candidates/$candid/$session/images/$minc_name/bids/$file_name-events.txt";
                 }
